b6a8d27e225cff4ced4cbbbd254ebdb9ade46234
Doris Addai <doricyj@gmail.com>
2024-03-30 15:06:39 +0000

    Added styling for error message

--- js/script.js
+++ js/script.js
@@ -1,25 +1,31 @@
 const form = document.getElementById('signup-form');
-const emailInput = document.getElementById('email');
-const emailError = document.getElementById('email-error');
+const emailInput = document.querySelector('.email-input');
+const errorText = document.querySelector('.error-message');
 const successMessage = document.getElementById('success-message');
+const errorColor = "hsl(4, 100%, 97%)";
 
 form.addEventListener('submit',submitEventHandler);
-
 
 function submitEventHandler (event) {
     event.preventDefault();
     if (!emailInput.value) {
-        emailError.textContent = 'Email is required';
+        setErrorMessage("Email is required",emailInput,errorText);
     } else if (isValidEmail(emailInput.value) === false) {
-        emailError.textContent = 'Valid email required';
+        setErrorMessage("Valid email required",emailInput,errorText);
     } else {
         successMessage.textContent = `Thank you for signing up! Your email ${emailInput.value} has been added to our newsletter list.`;
         successMessage.style.display = 'block';
-        emailError.textContent = '';
+        errorText.textContent = '';
         emailInput.value = '';
     }
 }
 
+function setErrorMessage(errorMessage,element,errorElement){
+    errorElement.textContent = errorMessage;
+    element.style.backgroundColor = errorColor;
+    element.style.border = `1px solid hsl(4, 100%, 67%)`;
+    element.style.color = "hsl(4, 100%, 67%)";
+}
 function isValidEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
