06f20eadeae22e6684aa5dbe4c083962c67286f9
Doris Addai <doricyj@gmail.com>
2024-03-30 11:38:53 +0000

    Refactored event handling

--- js/script.js
+++ js/script.js
@@ -3,19 +3,21 @@ const emailInput = document.getElementById('email');
 const emailError = document.getElementById('email-error');
 const successMessage = document.getElementById('success-message');
 
-form.addEventListener('submit', function(event) {
+form.addEventListener('submit',submitEventHandler);
+
+function submitEventHandler (event) {
     event.preventDefault();
     if (!emailInput.value) {
         emailError.textContent = 'Email is required';
-    } else if (!isValidEmail(emailInput.value)) {
-        emailError.textContent = 'Please enter a valid email address';
+    } else if (isValidEmail(emailInput.value) === false) {
+        emailError.textContent = 'Valid email required';
     } else {
         successMessage.textContent = `Thank you for signing up! Your email ${emailInput.value} has been added to our newsletter list.`;
         successMessage.style.display = 'block';
         emailError.textContent = '';
         emailInput.value = '';
     }
-});
+}
 
 function isValidEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
