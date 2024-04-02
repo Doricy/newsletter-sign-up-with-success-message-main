9396778643e498a7a9e1060d4fdff91f0fd648af
Doris Addai <doricyj@gmail.com>
2024-04-02 18:40:11 +0000

    Added response for mobile view

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ const emailInput = document.querySelector('.email-input');
 const errorText = document.querySelector('.error-message');
 const successMessage = document.getElementById('success-message');
 const errorColor = "hsl(4, 100%, 97%)";
+const popUp= document.querySelector('.popup');
+const container = document.querySelector('.container');
+
+
 
 form.addEventListener('submit',submitEventHandler);
 
@@ -13,11 +17,18 @@ function submitEventHandler (event) {
     } else if (isValidEmail(emailInput.value) === false) {
         setErrorMessage("Valid email required",emailInput,errorText);
     } else {
-        successMessage.textContent = `Thank you for signing up! Your email ${emailInput.value} has been added to our newsletter list.`;
-        successMessage.style.display = 'block';
+        console.log(popUp)
+        successMessage.textContent = `A confirmation email has been sent to ${emailInput.value}. Please open it and click the button inside to confirm your subscription.`;
         errorText.textContent = '';
         emailInput.value = '';
+        popUp.style.display ='unset';
+        container.style.display ='none'; 
     }
+}
+
+function dismissPopup(){
+    popUp.style.display = 'none';
+    container.style.display = 'grid';
 }
 
 function setErrorMessage(errorMessage,element,errorElement){
@@ -26,6 +37,7 @@ function setErrorMessage(errorMessage,element,errorElement){
     element.style.border = `1px solid hsl(4, 100%, 67%)`;
     element.style.color = "hsl(4, 100%, 67%)";
 }
+
 function isValidEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
